fa0817e63d8adf7627eacc9c151c8cade1f470e0
Danilo de Barros Ribeiro <nilo22@gmail.com>
2022-08-09 15:13:57 -0300

    req 18 livecode

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Ranking extends Component {
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="btn-go-home"
+          >
+            Home
+          </button>
+        </Link>
       </div>
     );
   }
